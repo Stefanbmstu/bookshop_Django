db4130f4e80e4cdbfb5fa08fad8227c9b226b7f0
Stefan Milevich <milevich.stefan@gmail.com>
2023-10-18 20:54:20 +0300

    permissiions

--- books/store/views.py
+++ books/store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from store.permissions import IsOwnerOrReadOnly
 from store.models import Book
 from store.serializers import BooksSerializer
 
@@ -10,10 +11,14 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        # serializer.validated_data['owner'] = self.request.user
+        serializer.save(owner=self.request.user)
 
 def auth(request):
     return render(request, 'oauth.html')
